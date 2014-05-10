0b606b06daae612ffa43dc16d9e4b734e21c736f
Pavel Shaydo <zwon@cpan.org>
2014-05-10 23:44:32 +0800

    add basic counter state implementation to EventStream

--- lib/Data/EventStream.pm
+++ lib/Data/EventStream.pm
@@ -1,6 +1,5 @@
 package Data::EventStream;
-use strict;
-use warnings;
+use Moose;
 our $VERSION = "0.02";
 $VERSION = eval $VERSION;
 
@@ -26,6 +25,73 @@ Collection of modules for processing streams of events.
 =head1 METHODS
 
 =cut
+
+has events => (
+    is      => 'ro',
+    traits  => ['Array'],
+    default => sub { [] },
+    handles => {
+        all_events   => 'elements',
+        push_event   => 'push',
+        shift_event  => 'shift',
+        count_events => 'count',
+        clear_events => 'clear',
+    },
+);
+
+has aggregate_states => (
+    is      => 'ro',
+    traits  => ['Array'],
+    default => sub { [] },
+    handles => {
+        push_state => 'push',
+    },
+);
+
+has length => ( is => 'ro', default => 0, writer => '_set_length' );
+
+sub add_state {
+    my ( $self, $state, %params ) = @_;
+    $params{_obj} = $state;
+    if ( $params{type} eq 'count' ) {
+        $params{_in} = 0;
+        if ( $params{length} > $self->length ) {
+            $self->_set_length( $params{length} );
+        }
+    }
+    $self->push_state( \%params );
+}
+
+sub add_event {
+    my ( $self, $event ) = @_;
+    my $ev     = $self->events;
+    my $ev_num = $self->count_events;
+    my $as     = $self->aggregate_states;
+
+    for my $state (@$as) {
+        if ( $state->{type} eq 'count' ) {
+            if ( $state->{_in} == $state->{length} ) {
+                $state->{on_out}->( $state->{_obj} ) if $state->{on_out};
+                $state->{_obj}->out( $ev->[ -$state->{_in} ] );
+                $state->{_in}--;
+            }
+        }
+    }
+
+    $self->push_event($event);
+
+    for my $state (@$as) {
+        if ( $state->{type} eq 'count' ) {
+            $state->{_obj}->in($event);
+            $state->{_in}++;
+            $state->{on_in}->( $state->{_obj} ) if $state->{on_in};
+        }
+    }
+
+    if ( $self->count_events > $self->length ) {
+        $self->shift_event;
+    }
+}
 
 1;
 
