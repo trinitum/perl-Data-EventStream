9ae7c165280f601100559451d3e5436872bcda6e
Pavel Shaydo <zwon@cpan.org>
2014-05-22 01:10:34 +0800

    bump version 0.05

--- lib/Data/EventStream.pm
+++ lib/Data/EventStream.pm
@@ -1,7 +1,7 @@
 package Data::EventStream;
 use 5.010;
 use Moose;
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 $VERSION = eval $VERSION;
 use Carp;
 use Data::EventStream::Window;
@@ -12,7 +12,7 @@ Data::EventStream - Perl extension for event processing
 
 =head1 VERSION
 
-This document describes Data::EventStream version 0.04
+This document describes Data::EventStream version 0.05
 
 =head1 SYNOPSIS
 
