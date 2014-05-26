060261083335b5de1fed17c390020a679c82a647
Pavel Shaydo <zwon@cpan.org>
2014-05-27 00:37:08 +0800

    document Data::EventStream::Statistics::Sample

--- lib/Data/EventStream/Statistics/Sample.pm
+++ lib/Data/EventStream/Statistics/Sample.pm
@@ -1,5 +1,32 @@
 package Data::EventStream::Statistics::Sample;
+use 5.010;
 use Moose;
+
+=head1 NAME
+
+Data::EventStream::Statistics::Sample - basic statistical functions for the sample
+
+=head1 SYNOPSIS
+
+    use Data::EventStream::Statistics::Sample;
+    my $stat = Data::EventStream::Statistics::Sample->new(
+        value_sub => \&event_value,
+    );
+    $ev_stream->add_aggregator($stat, %params);
+
+=head1 DESCRIPTION
+
+Module implements aggregator that calculates basic statistical functions for
+data set in aggregators' window.
+
+=head1 METHODS
+
+=head2 $class->new(value_sub => \&value_sub)
+
+Create a new aggregator. Requires I<value_sub> parameter which defines
+subroutine that returns numeric value for an event.
+
+=cut
 
 has value_sub => ( is => 'ro', required => 1, );
 
@@ -9,14 +36,38 @@ has _sq_sum => ( is => 'rw', default => 0, );
 
 has _count => ( is => 'rw', default => 0, );
 
+=head2 $self->count
+
+Current number of events in the window
+
+=cut
+
 sub count { shift->_count }
 
+=head2 $self->sum
+
+Sum of all events in the window
+
+=cut
+
 sub sum { shift->_sum }
+
+=head2 $self->mean
+
+Average value for the event
+
+=cut
 
 sub mean {
     my $self = shift;
     return $self->_count ? $self->_sum / $self->_count : undef;
 }
+
+=head2 $self->variance
+
+Variance of the data. Division by n-1 is used
+
+=cut
 
 sub variance {
     my $self  = shift;
@@ -27,10 +78,24 @@ sub variance {
     return $variance > 0 ? $variance : 0;
 }
 
+=head2 $self->standard_deviation
+
+Standard deviation of the data. Division by n-1 is used
+
+=cut
+
 sub standard_deviation {
     my $variance = shift->variance;
     return defined $variance ? sqrt($variance) : undef;
 }
+
+=head1 STANDARD AGGREGATOR METHODS
+
+=head2 $self->enter($event, $win)
+
+Invoked when event enters window
+
+=cut
 
 sub enter {
     my ( $self, $event, $window ) = @_;
@@ -40,6 +105,12 @@ sub enter {
     $self->_count( $self->_count + 1 );
 }
 
+=head2 $self->leave($event, $win)
+
+Invoked when event leaves window
+
+=cut
+
 sub leave {
     my ( $self, $event, $window ) = @_;
     my $val = $self->value_sub->($event);
@@ -48,12 +119,24 @@ sub leave {
     $self->_count( $self->_count - 1 );
 }
 
+=head2 $self->reset($win)
+
+Invoked when aggregator is reset
+
+=cut
+
 sub reset {
     my ( $self, $window ) = @_;
     $self->_sum(0);
     $self->_sq_sum(0);
     $self->_count(0);
 }
+
+=head2 $self->window_update($win)
+
+Invoked when window is updated
+
+=cut
 
 sub window_update {
     1;
